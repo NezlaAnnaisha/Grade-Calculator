23dec9b804a4fcad08e1c376f528da272a07044d
NezlaAnnaisha <nezla.annaisha@ucalgary.ca>
2023-02-11 08:38:12 +0000

    Grade calculator project after event handling was added.

---
+++ src/application/GradeCalculatorController.java
@@ -0,0 +1,50 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class GradeCalculatorController {
+
+    @FXML
+    private ChoiceBox<Integer> requiredCodingChallengesChoiceBox;
+
+    @FXML
+    private ChoiceBox<Integer> optionalCodingChallengesChoiceBox;
+
+    @FXML
+    private Slider quizSlider;
+
+    @FXML
+    private TextField projectGradeTextfield;
+    
+    @FXML
+    private Label courseGradeLabel;
+
+    @FXML
+    void calculateGrade(ActionEvent event) {
+    	double courseGrade = 0.0;
+    	String projectGrade = projectGradeTextfield.getText();
+    	courseGrade = Double.parseDouble(projectGrade) * 50/100;
+    	System.out.println("Project grade: " + projectGrade + ", Course grade so far: " + courseGrade);
+    	
+    	double quizGrade = quizSlider.getValue();
+    	courseGrade = courseGrade + (quizGrade * 100/10) * 0.25;
+    	System.out.println("Quiz grade: " + quizGrade + ", Course grade so far: " + courseGrade);
+    	
+    	int requiredCodingChallengesPassed = requiredCodingChallengesChoiceBox.getValue();
+    	courseGrade = courseGrade + (requiredCodingChallengesPassed * 100/20) * 0.25;
+    	System.out.println("Required coding challenges passed: " + requiredCodingChallengesPassed + ", Course grade so far: " + courseGrade);
+    	
+    	int optionalCodingChallengesPassed = optionalCodingChallengesChoiceBox.getValue();
+    	courseGrade = courseGrade + (optionalCodingChallengesPassed * 100/20) * 0.25;
+    	System.out.println("Optional coding challenges passed: " + optionalCodingChallengesPassed + ", Course grade so far: " + courseGrade);
+    	
+    	courseGradeLabel.setText(String.format("Your course grade is %.2f", courseGrade));
+    }
+
+}
+
