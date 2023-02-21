f18cc84de2ef813ac7a18c42085d9a92e1fd0383
NezlaAnnaisha <nezla.annaisha@ucalgary.ca>
2023-02-21 06:57:03 +0000

    The 'count' method passed all tests (Step #6).

--- src/basicjava/CCStringsIfAndWhile.java
+++ src/basicjava/CCStringsIfAndWhile.java
@@ -13,7 +13,26 @@ public class CCStringsIfAndWhile {
 	}
 
 	public static int count(String str, String chars) {	
-		return 0;
+		
+		int totalCount = 0;
+		
+		// Converts possible upper-case letters to lower-case
+		String newChars = chars.toLowerCase();
+		String newStr = str.toLowerCase();
+		
+		// Find what characters exists in 2nd string
+		for (char s2 : newChars.toCharArray()) {
+			
+			// Check for similar characters in 1st string
+			for (char s1 : newStr.toCharArray()) {
+				if (s1 == s2) {
+					totalCount++;
+				}				
+			}		
+		}
+		
+		
+		return totalCount;
 	}
 
 	public static int smallestDigit(int num) {		
