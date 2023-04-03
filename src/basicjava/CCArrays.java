e7d4611178cab9c9a547bceec9d380cb59731511
Nezla Annaisha <nezla.annaisha@ucalgary.ca>
2023-04-03 16:00:15 +0000

    Update changes on .java file.

--- src/basicjava/CCArrays.java
+++ src/basicjava/CCArrays.java
@@ -1,9 +1,27 @@
 package basicjava;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CCArrays {
 	
 	public static void replace(char[] charArray, char toReplace, char replaceWith) {
 		
+		for (char i : charArray) {
+			
+			//char[] copy = charArray.clone();
+			
+			//bypass case sensitivity
+			//set default to all char being lower case
+			
+			if (Character.toLowerCase(i) == toReplace) {
+				Arrays.copyOfRange(charArray, replaceWith, toReplace);
+			}
+			
+		
+		}
+			
+	
 	}
 	
 	public static void sortAlphabetic(String[] strArray) {
