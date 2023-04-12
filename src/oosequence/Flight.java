b2f8fea5a09e87c484f71014ce76c012ee5b1e3f
Nezla Annaisha <nezla.annaisha@ucalgary.ca>
2023-04-12 10:08:30 +0000

    Step 3 commit of 'Intro to Classes' CC.

---
+++ src/oosequence/Flight.java
@@ -0,0 +1,21 @@
+package oosequence;
+
+import java.util.Date;
+
+public class Flight {
+	Date departure;
+	Date arrival;
+	
+	Flight(Date departure, Date arrival){
+		
+	}
+	
+	Flight(Flight newInstance){
+		
+	}
+	
+	long length() {
+		return 0;
+	}
+	
+}
