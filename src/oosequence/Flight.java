1a6213094c95f48e08fa8d3724216eb6acf7a146
Nezla Annaisha <nezla.annaisha@ucalgary.ca>
2023-04-12 19:36:35 +0000

    Passed the constructor tests for 'Intro To Classes' CC.

--- src/oosequence/Flight.java
+++ src/oosequence/Flight.java
@@ -5,17 +5,42 @@ import java.util.Date;
 public class Flight {
 	Date departure;
 	Date arrival;
-	
-	Flight(Date departure, Date arrival){
-		
+
+
+	public Flight(Date departureTime, Date arrivalTime){
+		departure = departureTime;
+		arrival = arrivalTime;
+
+		// When either one or both arguments are null, set to instance variables directly
+		if (departureTime == null || arrivalTime == null) {
+			departure = departureTime;
+			arrival = arrivalTime;
+		}
+
+		// When neither arguments are null, check if departure is before arrival time
+		if (departureTime != null && arrivalTime != null){
+
+			// Set departure and arrival to null if departure comes before arrival time
+			if (departureTime.before(arrivalTime) == false) {
+				departure = null;
+				arrival = null;
+			}						
+		}
 	}
-	
-	Flight(Flight newInstance){
-		
+
+
+	public Flight(Flight copy){
+
 	}
-	
-	long length() {
+
+
+	public long length() {		
 		return 0;
 	}
-	
+
+
+
+
+
 }
+
