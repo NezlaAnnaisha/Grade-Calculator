82b4489ba579096ff649421506556e701452dbe9
Nezla Annaisha <nezla.annaisha@ucalgary.ca>
2023-04-13 00:33:17 +0000

    All tests for 'Flight' Class passed.

--- src/oosequence/Flight.java
+++ src/oosequence/Flight.java
@@ -3,8 +3,8 @@ package oosequence;
 import java.util.Date;
 
 public class Flight {
-	Date departure;
-	Date arrival;
+	private Date departure;
+	private Date arrival;
 
 
 	public Flight(Date departureTime, Date arrivalTime){
@@ -54,27 +54,65 @@ public class Flight {
 	}
 
 
-	public Object getDeparture() {
-		
+	public Date getDeparture() {		
 		return departure;
 	}
 
 
-	public Object getArrival() {
-		
+	public Date getArrival() {		
 		return arrival;
 	}
 
 
-	public void setDeparture(Date date) {
+	public void setDeparture(Date departure) {
 		
-		
+		// New departure time should only replace original departure IF departure is before arrival
+		// Check if variables are nulls
+		if (this.departure != null && this.arrival != null) {		
+					
+			// Check if departure is before arrival
+			if (this.departure.before(arrival)) { 
+						
+				if (departure.before(this.departure)) {
+					this.departure = departure;
+				} else {
+					departure = this.departure;
+				}
+						
+			} else {					
+				this.departure = departure;
+			}
+						
+		} 
+					
+		this.departure = departure;
+			
 	}
 
 
-	public void setArrival(Date date) {
+	public void setArrival(Date arrival) {
+
+		// New arrival time should only replace original arrival IF arrival is after departure
+		// Check if variables are nulls
+		if (this.arrival != null && this.departure != null) {					
+			
+			// Check if arrival is after departure
+			if (this.arrival.after(departure)) { 				
+				
+				if (arrival.after(this.arrival)) {
+					this.arrival = arrival;
+				} else {
+					arrival = this.arrival;
+				}
+				
+			} else {				
+				this.arrival = arrival;
+			}
+								
+		} 		
 		
-		
+		this.arrival = arrival;	
+
 	}
 
 
