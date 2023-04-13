e3b2b07be8641bf8cbc36bf64beda52bb2d93156
Nezla Annaisha <nezla.annaisha@ucalgary.ca>
2023-04-13 05:34:00 +0000

    Flight sub-class created (step 12 of CC).

---
+++ src/oosequence/Flight.java
@@ -0,0 +1,28 @@
+package oosequence;
+
+public class Flight extends TripComponent {
+	private String departureAirport;
+	private String arrivalAirport;
+
+	public String getDepartureAirport() {
+		return departureAirport;
+	}
+
+	public void setDepartureAirport(String departureAirport) {
+		this.departureAirport = departureAirport;
+	}
+
+	public String getArrivalAirport() {
+		return arrivalAirport;
+	}
+
+	public void setArrivalAirport(String arrivalAirport) {
+		this.arrivalAirport = arrivalAirport;
+	}
+
+	public String getDuration() {
+		return "";
+	}
+
+
+}
